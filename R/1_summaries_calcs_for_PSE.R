cde6274a748acf4a488dba59ead07e799d4a13d1
Eric Hertz <ehertz@psf.ca>
2021-05-25 08:41:57 -0700

    80% cutoff now working

--- R/1_summaries_calcs_for_PSE.R
+++ R/1_summaries_calcs_for_PSE.R
@@ -4,23 +4,18 @@
 rm(list = ls(all=TRUE)); #Remove all the objects in the memory
 # One time command to set the ProgRegSummaries as the highest level directory
 #here::set_here()
-setwd(here::here())
-#setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/data")
-# Delete plyr and reshape3 if not loading them doesn't break your code lol, load as necessary
-#library(plyr)
-#require(reshape2)  # see if you even need this anymore
+#setwd(here::here())
+setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/data")
 library(tidyverse)  # good default load capturing ggplot2 and dplyr
 library(broom)
 library(geosphere)
 
-
-
 #read in CU level data
-cc_file <- read.csv("data/dataset_1part1.Dec072020_CC.csv", header = T)
-fraser_file <- read.csv("data/dataset_1part1.Jul282020_Fraser.csv", header = T)
-vimi_file <- read.csv("data/dataset_1part1.Dec082020_VIMI.csv", header = T)
-nass_file <- read.csv("data/dataset_1part1.Dec092020_Nass.csv", header = T)
-skeena_file <- read.csv("data/dataset_1part1.Dec092020_Skeena.csv", header = T)
+cc_file <- read.csv("dataset_1part1.Dec072020_CC.csv", header = T)
+fraser_file <- read.csv("dataset_1part1.Jul282020_Fraser.csv", header = T)
+vimi_file <- read.csv("dataset_1part1.Dec082020_VIMI.csv", header = T)
+nass_file <- read.csv("dataset_1part1.Dec092020_Nass.csv", header = T)
+skeena_file <- read.csv("dataset_1part1.Dec092020_Skeena.csv", header = T)
 
 # combine files from each region
 cu_dat <- rbind(cc_file,fraser_file,vimi_file,nass_file,skeena_file)
@@ -32,24 +27,23 @@ cu_dat$Species[cu_dat$Species=="River Sockeye"] <- "Sockeye"
 cu_dat$Species[cu_dat$Species=="Lake Sockeye"] <- "Sockeye"
 
 
-# Attempt at selecting 80% runs?
+# Selecting only the years that have CUs that comprise 80% of the maximum historical run size
 # ------------------------------------------------------------------------------
-cu_dat <- as_tibble(cu_dat)  # tibble print method is prettier
+cu_dat <- as_tibble(cu_dat) 
 
 max_runs <- 
     cu_dat %>% 
-    drop_na(Total.run) %>%  # tidyverse alternative for 
+    drop_na(Total.run) %>%  
     group_by(CUID, Species) %>% 
     slice(which.max(Total.run)) %>% 
     dplyr::rename(max_run = Total.run) %>%
-    ungroup()  # don't forget this step as this can bite you later
+    ungroup()  
 
 total_max_run <- 
   max_runs %>% 
   group_by(Species, Region) %>% 
   summarise(total_max_run = sum(max_run)) %>% 
   ungroup()
-
 
 test <- 
 cu_dat %>% 
@@ -72,28 +66,31 @@ years_min80 <-
     slice(which(cutoff == TRUE)) %>% 
     select(Species, Year, Region)
 
-# Can lop off the c1:cutoff if you want
 output <- left_join(years_min80, test)
 
 # ------------------------------------------------------------------------------
   
 #create provincial run size summary file 1, raw and relative run size by species, region, year
 
+## create column for raw run size
 d1<- na.omit(cu_dat) %>% 
   group_by(., Species, Region, Year) %>% 
-  summarise(prov_runsize_raw = sum(Total.run)) ## raw run size
+  summarise(prov_runsize_raw = sum(Total.run)) 
 
-
+## create column for relative run size
 dat2 <- d1 %>% 
   group_by(Species,Region) %>%
   mutate(prov_runsize_relative = (prov_runsize_raw / max(prov_runsize_raw))*100) #relative run size      
 
-#setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/output")
-write.csv(dat2, "output/Prov_runsize_1_20210521.csv", row.names=FALSE)
+## join files so that only years with greater than 80% of historical CUs are shown
+dat2 <- inner_join(dat2,years_min80)
+
+setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/output")
+write.csv(dat2, "Prov_runsize_1_20210521.csv", row.names=FALSE)
 
 #create provincial run size summary file 2, start year, end year, percent change
 
-#setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/data")
+setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/data")
 
 dat3 <- dat2 %>% group_by(Species,Region) %>%
   summarize(enddecade_year = max(Year))
@@ -107,6 +104,6 @@ dat4 <- dat2 %>%
 
 dat5 <- left_join(dat3, dat4, by=c("Region","Species"))
 
-#setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/output")
-write.csv(dat2, "output/Prov_runsize_2_20210521.csv", row.names=FALSE)
+setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/output")
+write.csv(dat2, "Prov_runsize_2_20210521.csv", row.names=FALSE)
 
