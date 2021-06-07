56e8fdda1b5abdbd729db1252b100fdfafae8642
Eric Hertz <ehertz@psf.ca>
2021-06-07 10:13:58 -0700

    Changed pink (odd) and pink (even) to pink

--- R/1_summaries_calcs_for_PSE.R
+++ R/1_summaries_calcs_for_PSE.R
@@ -94,6 +94,9 @@ setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and A
 fraser_dat <- read.csv("Fraser_total_runsize.csv", header = T) # add in aggregated fraser data
 dat2 <- rbind(dat2, fraser_dat)
 
+dat2$Species[dat2$Species=="Pink (Odd)"] <- "Pink"
+dat2$Species[dat2$Species=="Pink (Even)"] <- "Pink"
+
 write.csv(dat2, "Prov_runsize_1_20210521.csv", row.names=FALSE)
 
 #create provincial run size summary file 2, start year, end year, percent change
@@ -138,6 +141,9 @@ dat8 <- inner_join(dat8,years_min80)
 
 fraser_dat_reg <- read.csv("Fraser_total_runsize_reg.csv", header = T) # add in aggregated fraser data
 dat8 <- rbind(dat8, fraser_dat_reg)
+
+dat8$Species[dat8$Species=="Pink (Odd)"] <- "Pink"
+dat8$Species[dat8$Species=="Pink (Even)"] <- "Pink"
 
 write.csv(dat8, "Reg_runsize_1_20210525.csv", row.names=FALSE)
 
