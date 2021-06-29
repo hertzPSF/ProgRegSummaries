79b29a93e437369596603c03e421b69ed2657b78
J Dunic <jdunic@gmail.com>
2021-06-29 10:50:04 -0700

    use relative paths

--- R/1_summaries_calcs_for_PSE.R
+++ R/1_summaries_calcs_for_PSE.R
@@ -5,7 +5,7 @@ rm(list = ls(all=TRUE)); #Remove all the objects in the memory
 # One time command to set the ProgRegSummaries as the highest level directory
 #here::set_here()
 #setwd(here::here())
-setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/data")
+setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries")
 library(tidyverse)
 library(broom)
 library(geosphere)
@@ -14,11 +14,11 @@ library(geosphere)
 #### 1 Read in and manipulated data
 
 #read in CU level data
-cc_file <- read.csv("dataset_1part1.Dec072020_CC.csv", header = T)
-fraser_file <- read.csv("dataset_1part1.Jul282020_Fraser.csv", header = T)
-vimi_file <- read.csv("dataset_1part1.Dec082020_VIMI.csv", header = T)
-nass_file <- read.csv("dataset_1part1.Dec092020_Nass.csv", header = T)
-skeena_file <- read.csv("dataset_1part1.Dec092020_Skeena.csv", header = T)
+cc_file <- read.csv("data/dataset_1part1.Dec072020_CC.csv", header = T)
+fraser_file <- read.csv("data/dataset_1part1.Jul282020_Fraser.csv", header = T)
+vimi_file <- read.csv("data/dataset_1part1.Dec082020_VIMI.csv", header = T)
+nass_file <- read.csv("data/dataset_1part1.Dec092020_Nass.csv", header = T)
+skeena_file <- read.csv("data/dataset_1part1.Dec092020_Skeena.csv", header = T)
 
 # combine files from each region
 cu_dat <- rbind(cc_file,fraser_file,vimi_file,nass_file,skeena_file)
@@ -91,19 +91,19 @@ d2 <- d1 %>%
 ## join files so that only years with greater than 80% of historical CUs are shown
 dat2 <- inner_join(d2,years_min80)
 
-setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/output")
+#setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/output")
 
-fraser_dat <- read.csv("Fraser_total_runsize.csv", header = T) # add in aggregated fraser data
+fraser_dat <- read.csv("output/Fraser_total_runsize.csv", header = T) # add in aggregated fraser data
 dat2 <- rbind(dat2, fraser_dat)
 
 dat2$Species[dat2$Species=="Pink (Odd)"] <- "Pink"
 dat2$Species[dat2$Species=="Pink (Even)"] <- "Pink"
 
-write.csv(dat2, "Prov_runsize_1_20210521.csv", row.names=FALSE)
+write.csv(dat2, "output/Prov_runsize_1_20210521.csv", row.names=FALSE)
 
 #create provincial run size summary file 2, start year, end year, percent change
 
-setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/data")
+#setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/data")
 
 dat3 <- dat2 %>% group_by(Species,Region) %>%
   summarize(enddecade_year = max(Year))
@@ -130,8 +130,8 @@ dat2 %>%
 
 dat5 <- left_join(dat3, dat4, by=c("Region","Species"))
 
-setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/output")
-write.csv(dat5, "Prov_runsize_2_20210521.csv", row.names=FALSE)
+#setwd("~/Dropbox (Salmon Watersheds)/X Drive/1_PROJECTS/Population Methods and Analysis/ProgRegSummaries/output")
+write.csv(dat5, "output/Prov_runsize_2_20210521.csv", row.names=FALSE)
 
 #### 3 Generate files needed for regional summaries
 #calculate average of run size for each region and species by year
@@ -157,5 +157,5 @@ dat8 <- inner_join(dat8,years_min80)
 dat8$Species[dat8$Species=="Pink (Odd)"] <- "Pink"
 dat8$Species[dat8$Species=="Pink (Even)"] <- "Pink"
 
-write.csv(dat8, "Reg_runsize_1_20210525.csv", row.names=FALSE)
+write.csv(dat8, "output/Reg_runsize_1_20210525.csv", row.names=FALSE)
 
